e730b9a219b6bf80f49b56ce8578e84089e77d72
Marco <marco14lyoko@gmail.com>
2022-05-17 18:06:43 +0100

    Adding useful algorithms

--- src/graph.cpp
+++ src/graph.cpp
@@ -94,3 +94,166 @@ list<int> Graph::dijkstra_path(int a, int b) {
 }
 
 
+// Depth-First Search: example implementation
+int Graph::dfs(int v) {
+    cout << v << " "; // show node order
+    int count = 1;
+    nodes[v].visited = true;
+    for (auto e: nodes[v].adj) {
+        int w = e.dest;
+        if (!nodes[w].visited) {
+            count += dfs(w);
+        }
+    }
+    return count;
+}
+
+// Breadth-First Search: example implementation
+void Graph::bfs(int v) {
+    nodes[v].dist = 0;
+    for (int i = 1; i <= n; i++) nodes[i].visited = false;
+    queue<int> q; // queue of unvisited nodes
+    q.push(v);
+    nodes[v].visited = true;
+    while (!q.empty()) { // while there are still unvisited nodes
+        int u = q.front();
+        q.pop();
+        cout << u << " "; // show node order
+        for (auto e: nodes[u].adj) {
+            int w = e.dest;
+            if (!nodes[w].visited) {
+                q.push(w);
+                nodes[w].visited = true;
+                nodes[w].dist = nodes[u].dist + 1;
+            }
+        }
+    }
+}
+
+
+int Graph::outDegree(int v) {
+    if (v < 1 || v > n) return -1;
+    return (int) nodes[v].adj.size();
+}
+
+
+int Graph::connectedComponents() {
+    int count = 0;
+    for (int i = 1; i <= n; i++)
+        if (!nodes[i].visited) {
+            count++;
+            dfs(i);
+        }
+    return count;
+}
+
+
+int Graph::giantComponent() {
+    int maxSize = -1;
+    for (int i = 1; i <= n; i++)
+        if (!nodes[i].visited)
+            maxSize = max(dfs(i), maxSize);
+    return maxSize;
+}
+
+
+list<int> Graph::topologicalSorting() {
+
+    for (int i = 1; i <= n; i++)
+        nodes[i].visited = false;
+
+    list<int> order;
+
+    for (int i = 1; i <= n; i++)
+        if (!nodes[i].visited)
+            dfsTopSort(i, order);
+
+    return order;
+}
+
+void Graph::dfsTopSort(int v, list<int> &l) {
+    nodes[v].visited = true;
+    for (auto e: nodes[v].adj) {
+        int w = e.dest;
+        if (!nodes[w].visited)
+            dfsTopSort(w, l);
+    }
+    l.push_front(v);
+}
+
+
+int Graph::distance(int a, int b) {
+    if (a == b) return 0;
+    for (int i = 1; i <= n; i++)
+        nodes[i].dist = -1;
+    bfs(a);
+    return nodes[b].dist;
+}
+
+
+int Graph::diameter() {
+    if (connectedComponents() > 1) return -1;
+
+    bfs(1);
+    int d = 0;
+
+    for (int i = 1; i <= n; i++)
+        d = max(d, nodes[i].dist);
+
+    return d;
+
+}
+
+
+bool Graph::hasCycle() {
+    for (int i = 1; i <= n; i++)
+        nodes[i].color = WHITE;
+    return cycleDfs(1);
+}
+
+bool Graph::cycleDfs(int v) {
+    nodes[v].color = GREY;
+    for (auto e: nodes[v].adj) {
+        int w = e.dest;
+        if (nodes[w].color == GREY) return true;
+        else if (nodes[w].color == WHITE)
+            return cycleDfs(w);
+    }
+    nodes[v].color = BLACK;
+    return false;
+}
+
+
+int Graph::prim(int r) {
+
+    MinHeap<int, int> heap(n, -1);
+
+    for (int i = 1; i <= n; i++) {
+        nodes[i].dist = INF;
+        nodes[i].pred = -1;
+        heap.insert(i, nodes[i].dist);
+    }
+
+    nodes[r].dist = 0;
+    heap.decreaseKey(r, 0);
+
+    int cost = 0;
+    while (heap.getSize() > 0) {
+
+        int u = heap.removeMin();
+        cost += nodes[u].dist;
+
+        for (Edge edge: nodes[u].adj) {
+
+            int peso = edge.duration;
+            int dest = edge.dest;
+
+            if (heap.hasKey(dest) and nodes[dest].dist > peso) {
+                nodes[dest].dist = peso;
+                nodes[dest].pred = u;
+                heap.decreaseKey(dest, peso);
+            }
+        }
+    }
+    return cost;
+}
