7468b0b2d57428f81964bdad8b78cf66311c7167
Johnny-droid <joaoricardoralves@gmail.com>
2022-05-21 12:12:29 +0100

    Added graphs for test max capacity and created test file

---
+++ Tests/maxCapacityTests.cpp
@@ -0,0 +1,20 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <iostream>
+#include "../include/graph.h"
+
+using namespace std;
+
+TEST(MaxCapacity, cenario_1_1) {
+    cout << "\tTesting MaxCapacity - cenario_1_1\n" << endl;
+
+    Graph graph1 = buildGraph(99, true);
+    Graph graph2 = buildGraph(98, true);
+
+
+
+}
+
+
+
+
