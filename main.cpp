4d27540f6acc646eaf14fb033a4f3956455f8c85
Marco <marco14lyoko@gmail.com>
2022-05-31 22:30:53 +0100

    Menu improvements

--- main.cpp
+++ main.cpp
@@ -1,16 +1,8 @@
-#include <iostream>
-#include "include/graph.h"
 #include "include/menu.h"
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
-
-    // Menu::start();
-
-    Graph graph = buildGraph(95, true);
-    int maxFlow = graph.edmonds_karp();
-    cout << "Max Flow: " << maxFlow;
-
+    Menu::start();
     return 0;
 }
