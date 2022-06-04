aaee20595b6e12bd383031a8965a6e53a12894d0
Johnny-droid <joaoricardoralves@gmail.com>
2022-06-04 11:11:50 +0100

    Added comments for documentation

--- include/graph.h
+++ include/graph.h
@@ -45,34 +45,100 @@ class Graph {
     vector<Node> nodes;
 
 public:
-
+    /**
+     * Builds a graph
+     * @param nodes
+     * @param includeResidual
+     */
     explicit Graph(int nodes, bool includeResidual = false);
 
+    /**
+     * Builds the transpose graph
+     * @return transposed graph
+     */
     Graph *transpose();
 
+    /**
+     * Adds an edge to the graph that connects src and dest
+     * @param src
+     * @param dest
+     * @param duration
+     * @param cap
+     */
     void addEdge(int src, int dest, int duration, int cap = 1);
 
+    /**
+     * Calculates a maximum capacity path for the graph
+     * @param a
+     * @param b
+     * @return max capacity
+     */
     int maximum_capacity(int a, int b);
 
+    /**
+     * Among the maximum capacity paths,
+     * it calculates the shortest one
+     * @param a
+     * @param b
+     * @param max
+     * @return max capacity
+     */
     int maximum_capacity_with_shortest_path(int a, int b, int max = INT32_MAX);
 
+    /**
+     * Among the shortest paths, it calculates
+     * the path with the highest capacity
+     * @param a
+     * @param b
+     * @param min
+     * @return capacity
+     */
     int shortest_path_with_maximum_capacity(int a, int b, int min = INT32_MIN);
 
+    /**
+     * Gets the path of the last used algorithm
+     * @param a
+     * @param b
+     * @return path
+     */
     list<int> get_path(int a, int b);
 
+    /**
+     * It traverses the graph using a depth-first search
+     * @param v
+     * @return number of nodes
+     */
     int dfs(int v);
+
 
     bool bfs_sink(int u, int v);
 
     // stack<int> topologicalSorting();
 
+    /**
+     * Calculates de topological sorting path using dfs
+     * @param v
+     * @param l
+     */
     void dfsTopSort(int v, stack<int> &l);
 
+    /**
+     * Checks the graph for cycles
+     * @param v
+     * @return
+     */
     bool cycleDfs(int v);
 
+    /**
+     * Prints the earliest start of the last node
+     */
     void print_readyAt();
 
+    /**
+     * Calculates the critical path of the graph using latest finish
+     */
     void critical_path_lf();
+
 
     void max_FL();
 
@@ -80,8 +146,20 @@ public:
 
     void max_path_dag();
 
+    /**
+     * Checks if there is a shortest path with the capacity higher or equal
+     * than the one given. If so, then it outputs the result to the user.
+     * @param groupSize
+     * @return true if path found, false otherwise
+     */
     bool cen_2_1(int groupSize);
 
+    /**
+     * Bfs used to calculate edmonds_karp_algorithm
+     * @param s
+     * @param t
+     * @return 1 if node t is reached, false otherwise
+     */
     int edmonds_karp_bfs(int s, int t);
 
     int dinic_algo();
